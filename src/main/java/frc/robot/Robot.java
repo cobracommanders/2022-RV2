38138026bb0e72ed23fffe40375793b90a431a2e
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-08-25 21:18:22 -0700

    remove unneeded command in robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-	private Command m_autonomousCommand;
 
 	@Override
 	public void robotInit() {
