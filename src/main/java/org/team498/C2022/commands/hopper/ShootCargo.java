a58bfa6697992d25e1d99c548c84eee6dc2d18b2
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-10-27 19:53:34 -0700

    Make shoot cargo more reliable and have it shoot two cargo

--- src/main/java/org/team498/C2022/commands/hopper/ShootCargo.java
+++ src/main/java/org/team498/C2022/commands/hopper/ShootCargo.java
@@ -8,6 +8,7 @@ import org.team498.C2022.subsystems.Hopper.HopperSetting;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 public class ShootCargo extends SequentialCommandGroup {
@@ -20,12 +21,21 @@ public class ShootCargo extends SequentialCommandGroup {
 					public Command get() {
 						if (hopper.getLowerSensor()) {
 							// new SequentialCommandGroup(
-							return new SaveCargoHigh(hopper);
-							// );
+							// return new SaveCargoHigh(hopper);
+							return new SequentialCommandGroup(
+									new SetHopper(hopper, HopperSetting.LOAD),
+									new WaitCommand(0.35),
+									new SetHopper(hopper, HopperSetting.IDLE),
+									new WaitCommand(1),
+									new SetHopper(hopper, HopperSetting.LOAD),
+									new WaitCommand(0.5),
+									new SetHopper(hopper, HopperSetting.IDLE));
+									// TODO potentially set the hopper to 'reverse' for a few seconds to align the top cargo better
 						}
 						return new SetHopper(hopper, HopperSetting.IDLE);
 					}
-				})
+				}),
+				new SetHopper(hopper, HopperSetting.IDLE)
 				);
 		;
 	}
